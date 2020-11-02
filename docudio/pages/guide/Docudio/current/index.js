546d26e0b375f6b8774ff42e5dc6d60a0344ef1f
Miclain Keffeler <mickeffeler@gmail.com>
2020-11-02 10:10:45 -0600

    fixed pages

---
+++ docudio/pages/guide/Docudio/current/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Card, makeStyles, CardHeader, CardContent, Typography } from '@material-ui/core'
+import { withTranslation } from '../../i18n'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    marginBottom: '10px'
+  }
+}))
+
+function CurrentGuide ({ t }) {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title={t('cardtitle')} />
+      <CardContent>
+        <Typography align='center' variant='h3'>
+          {t('title')}
+        </Typography>
+        <Typography align='center' variant='h5' style={{ marginBottom: '30px' }}>
+          {t('statement')}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+CurrentGuide.getInitialProps = async () => ({
+  namespacesRequired: ['currentguide']
+})
+
+export default withTranslation('currentguide')(CurrentGuide)
--- docudio/pages/products/docudio.js
+++ docudio/pages/products/docudio.js
@@ -1,4 +1,29 @@
-<Typography variant='body1'>
+import React from 'react'
+import { Card, makeStyles, CardHeader, CardContent, Typography } from '@material-ui/core'
+import { withTranslation } from '../../i18n'
+import Link from 'next/link'
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    marginBottom: '10px'
+  }
+}))
+
+function DocudioProduct ({ t }) {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title={t('cardtitle')} />
+      <CardContent>
+        <Typography align='center' variant='h3'>
+          {t('title')}
+        </Typography>
+        <Typography align='center' variant='h5' style={{ marginBottom: '30px' }}>
+          {t('statement')}
+        </Typography>
+        <Typography variant='body1'>
   <Link
     activeClassName='Mui-selected'
     href='/downloads/docudio'
@@ -8,3 +33,14 @@
     {t('downloads')}
   </Link>
 </Typography>
+
+      </CardContent>
+    </Card>
+  )
+}
+
+DocudioProduct.getInitialProps = async () => ({
+  namespacesRequired: ['docudioproduct']
+})
+
+export default withTranslation('docudioproduct')(DocudioProduct)
